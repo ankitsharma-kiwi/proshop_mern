0caae8c3d1147ec1bcadecd4b1907e102c49e4de
ankitsharma-kiwi <ankit.sharma@kiwitech.com>
2024-08-29 17:13:14 +0530

    add to cart functionality.

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,14 +1,26 @@
-import { useParams } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { useParams, useNavigate } from 'react-router-dom'
 import { Link } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import { useGetProductDetailsQuery } from '../slices/productApiSlice'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
+import { addToCart } from '../slices/cartSlice'
+import { useState } from 'react'
 
 const ProductScreen = () => {
   const { id: productId } = useParams()
   const { data: product, isLoading, error } = useGetProductDetailsQuery(productId)
+  const dispatch = useDispatch()
+  const [qty, setQty] = useState(1)
+  const navigate = useNavigate()
+
+  const addToCartHandler = () => {
+    dispatch(addToCart({ ...product, qty }))
+    navigate('/cart')
+  }
+
 
   return (
     <>
@@ -56,8 +68,30 @@ const ProductScreen = () => {
                     </Row>
                   </ListGroup.Item>
 
+                  {product.countInStock > 0 && (
+                    <ListGroup.Item>
+                      <Row>
+                        <Col>Qty</Col>
+                        <Col>
+                          <Form.Control as="select" value={qty} onChange={(e) => setQty(e.target.value)}>
+                            {[...Array(product.countInStock).keys()].map((x) => (
+                              <option key={x + 1} value={x + 1}>
+                                {x + 1}
+                              </option>
+                            ))}
+                          </Form.Control>
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  )}
+
                   <ListGroup.Item>
-                    <Button className="btn-block" type="button" disabled={product.countInStock === 0}>
+                    <Button
+                      className="btn-block"
+                      type="button"
+                      disabled={product.countInStock === 0}
+                      onClick={addToCartHandler}
+                    >
                       Add To Cart
                     </Button>
                   </ListGroup.Item>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { apiSlice } from './slices/apiSlices';
+import { apiSlice } from './slices/apiSlices'
+import cartSliceReducr from './slices/cartSlice'
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
+    cart: cartSliceReducr,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apiSlice.middleware),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: process.env.NODE_ENV !== 'production',
 })
 
-export default store;
+export default store
